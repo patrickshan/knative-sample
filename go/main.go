0b788c0a7414f5b51b7b86e0414cad1dab93e46b
Patrick Shan <pshan@atlassian.com>
2019-05-01 09:24:15 +1000

    add a go sample app

---
+++ go/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	log.Print("application received a request")
+	target, defined := os.LookupEnv("TARGET")
+	if !defined {
+		target = "World"
+	}
+	fmt.Fprintf(w, "Hello %s!", target)
+}
